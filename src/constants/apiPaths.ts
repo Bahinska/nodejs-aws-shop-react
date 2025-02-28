2d536fcab50b4f36ddbf48af93027d0ccfa70e3c
Margo <97248807+Bahinska@users.noreply.github.com>
2025-02-28 12:47:30 +0200

    minor path fix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://vtws3odmm9.execute-api.eu-north-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://vtws3odmm9.execute-api.eu-north-1.amazonaws.com/dev",
+  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
